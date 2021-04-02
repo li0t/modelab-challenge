6a57ca8dd1223e57604bf0c15661186762b02381
li0t <leonardo@tiralinea.com>
2021-04-02 18:37:12 -0300

    Set default values in getNewDiscount function

--- client/modules/cart/store/modules/cart/services/get-new-discount.js
+++ client/modules/cart/store/modules/cart/services/get-new-discount.js
@@ -4,15 +4,15 @@
 
 /**
  * Builds a discount object to store in the cart
- * renaming the "discount" attribute to "amount". 
- * 
+ * renaming the "discount" attribute to "amount".
+ *
  * @param {Object} discount A discount object.
  * @returns {Object} A new cart discount.
  */
-export default function getNewDiscount(discount) {
+export default function getNewDiscount(discount={}) {
   return {
-    threshold: discount.threshold,
-    brand: discount.brand,
-    amount: discount.discount
+    threshold: discount.threshold || 0,
+    amount: discount.discount || 0,
+    brand: discount.brand || ''
   };
 }
