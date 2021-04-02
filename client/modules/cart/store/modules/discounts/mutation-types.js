7114f7cc322970067c44fc294b551ed16bee7e18
li0t <leonardo@tiralinea.com>
2021-04-02 17:27:15 -0300

    Add discounts store mutations

---
+++ client/modules/cart/store/modules/discounts/mutation-types.js
@@ -0,0 +1,9 @@
+/**
+ * @module cart/store/modules/discounts/mutation-types
+ */
+
+import createAsyncMutation from '@/shared/services/create-async-mutation';
+
+// Async
+export const FETCH_DISCOUNTS = createAsyncMutation('fetch', 'discounts');
+
