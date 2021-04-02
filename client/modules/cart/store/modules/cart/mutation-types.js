65bdaef8fb2faede3626f5feec1b041daf8d02d9
li0t <leonardo@tiralinea.com>
2021-04-02 13:36:23 -0300

    Add Cart store mutations

---
+++ client/modules/cart/store/modules/cart/mutation-types.js
@@ -0,0 +1,15 @@
+/**
+ * @module cart/store/mutation-types
+ */
+
+
+// Sync
+export const PUSH_PRODUCT = 'PUSH_PRODUCT';
+export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
+export const UPDATE_PROD_QTY = 'UPDATE_PROD_QTY';
+
+export const UPDATE_TOTAL = 'UPDATE_TOTAL';
+export const UPDATE_DISCOUNT = 'UPDATE_DISCOUNT';
+export const UPDATE_SUBTOTAL = 'UPDATE_SUBTOTAL';
+export const UPDATE_DISCOUNT_ALTERNATIVE = 'UPDATE_DISCOUNT_ALTERNATIVE';
+
---
+++ client/modules/cart/store/modules/cart/mutations.js
@@ -0,0 +1,61 @@
+/**
+ * @module cart/store/modules/cart/mutations
+ */
+
+import MT from '@/store/mutation-types';
+import getNewProduct from './services/get-new-product';
+
+export default {
+  [MT.PUSH_PRODUCT](state, product) {
+    const newProduct = getNewProduct(product);
+
+    state.products.push(newProduct);
+  },
+
+  [MT.SPLICE_PRODUCT](state, productId) {
+    const products = state.products;
+
+    const productIndex = products.findIndex(prod => prod.id === productId);
+
+    if (productIndex === -1) {
+      throw new Error(`Product ${productId} was not found`);
+    }
+
+    products.splice(productIndex, 1);
+  },
+
+  [MT.UPDATE_PROD_QTY](state, { productId, qty }) {
+    const products = state.products;
+
+    const product = products.find(prod => prod.id === productId);
+
+    if (!product) {
+      throw new Error(`Product ${productId} was not found`);
+    }
+
+    product.qty = qty;
+    product.subtotal = product.price * qty;
+  },
+
+  [MT.UPDATE_SUBTOTAL](state) {
+    let subtotal = 0;
+
+    for (const product of state.products) {
+      subtotal += product.subtotal;
+    }
+
+    state.subtotal = subtotal;
+  },
+
+  [MT.UPDATE_DISCOUNT](state, discount ) {
+    state.discount = discount;
+  },
+
+  [MT.UPDATE_DISCOUNT_ALTERNATIVE](state, discountAlternative ) {
+    state.discountAlternative = discountAlternative;
+  },
+
+  [MT.UPDATE_TOTAL](state) {
+    state.total = state.subtotal - state.discount.amount;
+  }
+};
