2b60a4736b96639c0d3b591f1e8d9fc82c372adb
li0t <leonardo@tiralinea.com>
2021-04-05 18:36:01 -0400

    Add CartSidebar styling ;)

--- client/modules/cart/components/CartSidebar.vue
+++ client/modules/cart/components/CartSidebar.vue
@@ -1,73 +1,92 @@
 <i18n>
 es:
+  SUMMARY: Resumen
+
+  EMPTY_CART: No se han agregado productos al carro
+
   PRODUCT_QTY: 'Cantidad: {0}'
   SUBTOTAL: Subtotal de productos
   DISCOUNT: Descuento por marca
+  TOTAL: Total a pagar
+
   DISCOUNT_APPLIED_0: '* Se aplicó un descuento de '
   DISCOUNT_APPLIED_1: ' por haber comprado al menos '
   DISCOUNT_APPLIED_2: ' en productos {0}!'
 
   DISCOUNT_ALTERNATIVE_0: 'Agrega '
-  DISCOUNT_ALTERNATIVE_1: ' más en tus productos {0} y aprovecha un descuento total de '
+  DISCOUNT_ALTERNATIVE_1: ' más en productos {0} y aprovecha un descuento total de '
   DISCOUNT_ALTERNATIVE_2: ' en tu compra!'
 
 </i18n>
 
 <template lang="pug">
-.cart-sidebar.ui.right.wide.sidebar.vertical.menu(
+.cart-sidebar.ui.right.very.wide.sidebar.vertical.menu(
   @click="$$('.cart-sidebar').sidebar('hide')"
   )
-  .menu
+  .title.menu
+    .header.item
+      h3.ui.blue.header {{ $t('SUMMARY') }}
+  
+  .products.menu
     .item(
+      v-if="products.length === 0"
+      )
+      h4.ui.header {{ $t('EMPTY_CART')}}
+
+    .product.item(
       v-for="product in products"
       )
       .ui.grid
-        .row
+        .middle.aligned.row
           .four.wide.column
             .ui.small.image
               img(:src="`https://${product.image}`")
 
-          .four.wide.column 
-            p {{ product.brand }}
-            p {{ product.description }}
-            p {{ $t('PRODUCT_QTY', [product.qty]) }}
+          .six.wide.column.text 
+            p.brand.bold {{ product.brand }}
+            p.description {{ product.description }}
+            p.qty.bold {{ $t('PRODUCT_QTY', [product.qty]) }}
 
-          .eight.wide.right.aligned.column
-            | {{ product.subtotal }}
+          .six.wide.right.aligned.column.bold.price
+            | {{ formatNumber(product.subtotal) }}
 
-  .menu
-    .discount-alternative(
+  .totals.menu
+    .discount-alternative.item(
       v-show="discountAlternative.amount > 0"
       )
       span {{ $t('DISCOUNT_ALTERNATIVE_0') }}
-      span.bold {{ discountAlternative.missing }}
+      span.bold {{ formatNumber(discountAlternative.missing) }}
+
       span {{ $t('DISCOUNT_ALTERNATIVE_1', [discountAlternative.brand]) }}
-      span.bold {{ discountAlternative.amount }}
+      span.bold {{ formatNumber(discountAlternative.amount) }}
+
       span {{ $t('DISCOUNT_ALTERNATIVE_2') }}    
 
     .subtotal.item
       div 
         span {{ $t('SUBTOTAL') }}
-        span.right {{ subtotal }}
+        span.right.bold {{ formatNumber(subtotal) }}
 
     .discount.item(
       v-show="discount.amount > 0"
       )
       div 
         span {{ $t('DISCOUNT') }}
-        span.right {{ discount.amount }}
+        span.right.bold {{ `-${formatNumber(discount.amount)}` }}
 
       div 
         span {{ $t('DISCOUNT_APPLIED_0') }}
-        span.bold {{ discount.amount }}
+        span.bold {{ formatNumber(discount.amount) }}
+
         span {{ $t('DISCOUNT_APPLIED_1') }}
-        span.bold {{ discount.threshold }}
+        span.bold {{ formatNumber(discount.threshold) }}
+
         span {{ $t('DISCOUNT_APPLIED_2', [discount.brand]) }}
       
     .total.item
       div 
         span {{ $t('TOTAL') }}
-        span.right {{ total }}
+        span.right.bold {{ formatNumber(total) }}
  
 </template>
 
@@ -99,6 +118,10 @@ export default {
       displayCart: MT.DISPLAY_CART
     }),
 
+    formatNumber(number) {
+      return `$${number.toLocaleString('es-CL')}`;
+    },
+
     initCartSidebar() {
       this.$$('.cart-sidebar').sidebar('setting', 'transition', 'overlay');
     },
@@ -116,13 +139,84 @@ export default {
 };
 </script>
 <style lang="sass" scoped>
-.cart-sidebar
+.cart-sidebar.vertical.menu
+  padding: 1.25em
+
   .menu
     .item
+      padding-left: 0
+      padding-right: 0
+
       .bold
         font-weight: bold
       
       .right
         float: right
+
+    &.title
+      .header.item
+        .header
+          border-bottom: 1px solid rgba(34,36,38,.15)
+          padding-bottom: .5em
+    
+    &.products
+      margin-top: 1em
+
+      box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5
+      background-color: #F5F5F5
+
+      .item
+        padding: 1em 2em
+
+        &.product
+          border-bottom: 1px solid rgba(34, 36, 38, 0.15)
+
+          .price
+            font-size: 1.25em
+            color: #202020
+
+          .text 
+            padding-left: 0
+
+            p
+              margin-bottom: 0
+              color: #202020
+
+              &.brand,
+              &.description
+                font-size: 1.2em
+
+              &.qty
+                font-size: 1.1em
+
+              &.description
+                color: #505050
+
+          &:last-child
+            border-bottom: none
+
+    
+    &.totals
+      padding-top: 2em
+
+      .item
+        font-size: 1.25em
+        color: #505050
+        margin-bottom: 1em
+
+        &.discount-alternative
+          color: #2185d0
+          padding-top: 0;
+          margin-top: -1em;
+
+        &.discount
+          div:first-child
+            margin-bottom: .5em
+
+        &.total
+          color: black
+
+      
+
 
 </style>
