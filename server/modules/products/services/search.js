960acd66a235e164c070ce70b860c8e2e0b0f5f7
li0t <leonardo@tiralinea.com>
2021-04-05 19:08:56 -0400

    Text search products by description

--- server/modules/products/services/search.js
+++ server/modules/products/services/search.js
@@ -3,5 +3,5 @@ const buildSearchRegEx = require('../../../shared/services/search-regex');
 module.exports = function buildSearch(text) {
   const regex = buildSearchRegEx(text);
 
-  return [{ name: regex }];
+  return [{ description: regex }];
 };
