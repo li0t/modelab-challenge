1431773fa2c382e075e30f6634785d437db40ae2
li0t <leonardo@tiralinea.com>
2021-04-05 19:08:13 -0400

    Remove clean tests util

--- server/core/services/test/index.js
+++ server/core/services/test/index.js
@@ -4,14 +4,8 @@
  * @module core/services/test
  */
 
-const $utils = require('./utils');
-
 describe('MODELAB-CHALLENGE', () => {
   before(async () => {
     await require('./utils/req')();
   });
-});
-
-after(async () => {
-  await $utils.clean();
 });
