a6d503acf30fd7a10b5b809b474de014418ccd1c
li0t <leonardo@tiralinea.com>
2021-04-06 16:05:48 -0400

    Remove unused discounts queries

--- server/modules/discounts/routes/index.js
+++ server/modules/discounts/routes/index.js
@@ -1,20 +1,9 @@
-const getQuerySort = require('../../../shared/services/get-query-sort');
-const buildSearch = require('../services/search');
-
-const SORT_PARAMS = ['name'];
-
 module.exports = (router, mongoose) => {
   const Discount = mongoose.model('discount');
 
   router.get('/api/discounts/count', async (req, res, next) => {
     try {
       const query = Discount.countDocuments();
-
-      if (req.query.text) {
-        const search = buildSearch(req.query.text);
-
-        query.or(search);
-      }
 
       const counted = await query;
 
@@ -25,20 +14,12 @@ module.exports = (router, mongoose) => {
   });
 
   router.get('/api/discounts', async (req, res, next) => {
-    const sort = getQuerySort(req, SORT_PARAMS);
     try {
       const query = Discount.find()
-        .sort(sort)
 
         .skip(req.query.skip)
 
         .limit(req.query.limit);
-
-      if (req.query.text) {
-        const search = buildSearch(req.query.text);
-
-        query.or(search);
-      }
 
       const discounts = await query;
 
@@ -47,5 +28,4 @@ module.exports = (router, mongoose) => {
       next(error);
     }
   });
-
 };
