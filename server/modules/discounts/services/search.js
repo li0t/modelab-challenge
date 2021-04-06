5d6ded475c11526fa7076ca9ba0431b910e4e7d0
li0t <leonardo@tiralinea.com>
2021-04-06 16:05:14 -0400

    Delete unused services

--- server/modules/discounts/services/search.js
+++
@@ -1,7 +0,0 @@
-const buildSearchRegEx = require('../../../shared/services/search-regex');
-
-module.exports = function buildSearch(text) {
-  const regex = buildSearchRegEx(text);
-
-  return [{ name: regex }];
-};
